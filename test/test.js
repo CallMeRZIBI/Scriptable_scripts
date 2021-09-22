6b53ee5afed54348c580c7c268d96b6097fa24f5
CallMeRZIBI <michalenko.jakub@gmail.com>
2021-09-22 12:59:14 +0200

    completed test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,3 @@
-async function createWidget(){
-    // Create new empty ListWidget instance
-    let listwidget = new ListWidget();
-
-    // Return the created widget
-    return listwidget;
-}
-
 let widget = await createWidget();
 
 // Check where the cript is running
@@ -17,3 +9,83 @@ if(config.runsInWidget){
     widget.presentMedium();
 }
 Script.complete();
+
+async function createWidget(){
+    // Create new empty ListWidget instance
+    let listwidget = new ListWidget();
+
+    // Set new background color
+    listwidget.backgroundColor = new Color("#000000");
+
+    // Add widget heading
+    let heading = listwidget.addText("🚀Next🚀");
+    heading.centerAlignText();
+    heading.font = Font.lightSystemFont(25);
+    heading.textColor = new Color("#ffffff");
+
+    // Spacer between heading and launch date
+    listwidget.addSpacer(15);
+
+    // Fetch newxt launch date
+    let launch = await getNextLaunch();
+    let launchDateTime = getLaunchDateTime(launch);
+
+    // Add the launch time to the widget
+    displayLaunchDateTime(listwidget, launchDateTime, launchDateTime.date_precision);
+
+    // Return the created widget
+    return listwidget;
+}
+
+async function getNextLaunch(){
+    // Query url
+    const url = "https://api.spacexdata.com/v4/launches/next"
+
+    // Initialize new request
+    const request = new Request(url);
+
+    // Execute the request and parse the response as json
+    const response = await request.loadJSON();
+
+    // Return the returned launch data
+    return response;
+}
+
+function getLaunchDateTime(launchData){
+    // Parse launch date to new date object
+    const launchDateTime = new Date(launchData.date_uct);
+    return launchDateTime;
+}
+
+function addDateText(stack, text){
+    let dateText = stack.addText(text);
+    dateText.centerAlignText();
+    dateText.font = Font.semiboldSystemFont(20);
+    dateText.textColor = new Color("#ffffff");
+}
+
+function displayLaunchDateTime(stack, launchDateTime, precision){
+    // Check if next launch date is precise enough and display different details based on the precision
+    if(precision == "hour"){
+
+        // Add launch date
+        const dateOptions = {year: "numeric", month:"2-digit", day:"2-digit"};
+        let datestring = launchDateTime.toLocaleDateString(undefined, dateOptions);
+        addDateText(stack, datestring);
+
+        // Add launch time
+        const timeOptions = {hour: "numeric", minute:"numeric"};
+        let timestring = launchDateTime.toLocaleTimeString(undefined, timeOptions);
+        addDateText(stack, timestring);
+
+    } else if(precision == day){
+
+        // Add launch date
+        const dateOptions = {year: "numeric", month:"2-digit", day:"2-digit"};
+        let datestring = launchDateTime.toLocaleDateString(undefined, dateOptions);
+        addDateText(stack, datestring);
+
+    } else{
+        addDateText(stack, "No day for next launch given");
+    }
+}
